e38cfa05f04ffe9811f578e3244fb026e13d0ef0
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-06-04 10:45:43 +0530

    populate review

--- backEnd/Controllers/ownerController.js
+++ backEnd/Controllers/ownerController.js
@@ -39,7 +39,9 @@ export const getSingleOwner = async (req, res) => {
   const id = req.params.id;
 
   try {
-    const owner = await Owner.findById(id).select("-password");
+    const owner = await Owner.findById(id)
+      .populate("reviews")
+      .select("-password");
 
     res.status(200).json({
       success: true,
@@ -53,28 +55,27 @@ export const getSingleOwner = async (req, res) => {
 
 export const getAllOwners = async (req, res) => {
   try {
-    const {query} = req.query;
+    const { query } = req.query;
     let owners;
 
-    if(query) {
-        owners = await Owner.find({
-            isApproved: "approved",
-            $or: [
-                {name:{ $regex: query, $options: "i"}},
-                {category: { $regex: query, $options: "i"}},
-            ],
-        }).select("-password");
-    }   else{
-        owners = await Owner.find({isApproved: "approved"}).select("-password");
+    if (query) {
+      owners = await Owner.find({
+        isApproved: "approved",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { category: { $regex: query, $options: "i" } },
+        ],
+      }).select("-password");
+    } else {
+      owners = await Owner.find({ isApproved: "approved" }).select("-password");
     }
 
-        res.status(200).json({
-            success: true,
-            message: "users found",
-            data: owners,
-        })
-
-    } catch (err) {
+    res.status(200).json({
+      success: true,
+      message: "users found",
+      data: owners,
+    });
+  } catch (err) {
     res.status(404).json({ success: false, message: "Not found" });
   }
 };
