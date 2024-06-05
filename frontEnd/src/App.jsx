11debbd96abdd116df5aacb4efc69a00c04093e6
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-06-05 18:52:42 +0530

    chat-bot

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Layout from './layout/Layout'
+import Chat from "./components/Chat.jsx";
 function App() {
 
   return (
     <>
       <div>
+        <Chat />
         <Layout />
         </div>
 
